8ffca220cb0e8c6544adee37cf47e13171d4c989
Cesar Rodriguez <streamlink777@live.com>
2018-12-04 20:39:57 -0500

    first time summit

--- HistoricalTimeStation.py
+++ HistoricalTimeStation.py
@@ -1,37 +1,50 @@
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
 
-# settings
-key_api = 'spbdxdqepugjhgeh4kbrn6t2sz7jfytm'
-code = 34
+''' this program uses the imformation of timestation beta API to download information and find whos employees where working
+in a specify moment in time'''
+
+
+################### settings ###########################
+with open('codeTimeStation.txt', 'r') as tcode:
+    key_api = tcode.read()
+CODE = 34
+########################################################
 
 
 class data_analizer:
 
-    ''' '''
+    '''this part we take the information from timestation website and start analyze it'''
 
     def __init__(self, location):
 
         self.location = location
-
         self.date1 = input('introduce Date YYYY-MM-DD -- > ')
         self.time_d = input('Time (24H - 12:00, 15:00) -- > ')
+        
         print('\nPLease Wait....')
-
         self.url = f"https://api.mytimestation.com/v0.1/reports/?api_key={key_api}" \
-              f"&Report_StartDate={self.date1}&Report_EndDate={self.date1}&id={code}&exportformat=csv"
+              f"&Report_StartDate={self.date1}&Report_EndDate={self.date1}&id={CODE}&exportformat=csv"
 
         self.data = pd.read_csv(self.url)
-
-        #  ['Date', 'Employee ID', 'Name', 'Department', 'Device', 'Time',
-        # 'Activity', 'Punch Method', 'Latitude', 'Longitude', 'IP Address','Notes']
-        
         self.time = self.time_format_checker()
-
         self.display_results()
 
+    def display_results(self):
+        '''Inicial analisis and control flow of the class'''
+
+        x = self.gettings_points(self.time)
+        total_of_workers = x.shape[0]
+        x.reset_index(inplace=True)
+        if x.empty:
+            pass
+        else:
+            print(x[['Date', 'Name', 'Department', 'Device', 'Time']].to_string(index=False))
+        print('\n\n the total of employees is {} at {}'.format(total_of_workers, self.time))
+
     def gettings_points(self, t):
+        '''this created a dataframe with the data for the expecify point in time when employees are actually working 
+            and return it'''
 
         group_name = self.data[self.data["Time"] <= t]
         duplicates_removed = group_name.drop_duplicates(subset="Name", keep='last')
@@ -39,20 +52,11 @@ class data_analizer:
         x = punch_in_df[punch_in_df.Department.str.contains(self.location)]
         return x
 
-    def display_results(self):
-
-        x = self.gettings_points(self.time)
-        total_of_workers = x.shape[0]
-        x.reset_index(inplace=True)
-
-        if x.empty:
-            pass
-        else:
-            print(x[['Date', 'Name', 'Department', 'Device', 'Time']].to_string(index=False))
-
-        print('\n\n the total of employees is {} at {}'.format(total_of_workers, self.time))
-
     def time_format_checker(self):
+
+        ''' sometimes the user just place single number for hours. making the program crash,
+        so i made this test to add a cero before the time giving and anticipate posible problems.
+        this program require military time (24h) '''
 
         if len(self.time_d) == len('2:00'):
             return f'0{self.time_d}'
@@ -60,6 +64,9 @@ class data_analizer:
             return self.time_d
 
     def export_result(self):
+
+        '''simple program to export the resolts in to csv file.'''
+
         x = self.gettings_points(self.time)
         file_name = 'historicalFor{}.csv'.format(self.date1)
         x.to_csv(file_name)
@@ -67,8 +74,9 @@ class data_analizer:
 
     def quick_report(self):
 
-        list_times = '6:00 10:00 12:15 14:00 15:45'.split()
+        '''this created a report in detail for the hours especify in the botton'''
 
+        list_times = '6:00 10:00 12:15 14:00 15:45'.split()
         for n, t in enumerate(list_times):
             x = self.gettings_points(t)
             file_name = '{}-{}-{}.csv'.format(self.date1, t.replace(':', '.'), n)
@@ -77,9 +85,13 @@ class data_analizer:
 
 
 def start_up():
+    ''' this function will control the flow of the whole Program.'''
 
     def continue_answer():
 
+        ''' this part will make sure that the question for "location" is ask only once.
+            normally user search many times in the same location.
+        '''
         ibk_data = data_analizer(location)
 
         print('\n--for new search press "enter"')
@@ -109,6 +121,7 @@ def start_up():
 
 
 if __name__ == '__main__':
+
     print(' Welcome Historical Time Station Search ')
     print('____________________________________________________________________________________________________ \n\n\n')
     start_up()
